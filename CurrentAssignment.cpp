a68e8c02eec1f244b99b1245e8626d79e8a24f9f
lincoln <lincolnbartlett@gmail.com>
2019-02-23 23:45:19 -0800

    feb23

--- CurrentAssignment.cpp
+++ CurrentAssignment.cpp
@@ -1,34 +1,135 @@
 #include <iostream>
 using namespace std;
 
+int GetNumOfNonWSCharacters(string input)
+{
+    int count = 0;
+    int length = input.length();
+    for (int i = 0; i < length; i++)
+    {
+        if (input.at(i) != 32)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int GetNumOfWords(string input)
+{
+    int count = 0;
+    int length = input.length();
+    for (int i = 0; i < length; i++)
+    {
+        if (input.at(i) == 32 && input.at(i - 1) != 32)
+        {
+            count++;
+        }
+
+        if (input.at(i) == '.'){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int FindText(string searchString, string input)
+{
+    int count = 0;
+    int length = input.length();
+    int buffer = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (buffer != input.find(searchString, i))
+        {
+            if (buffer != 0)
+            {
+                count++;
+            }
+
+            buffer = input.find(searchString, i);
+        }
+    }
+    return count;
+}
+/*
+void ReplaceExclamation(string input)
+{
+}
+
+void ShortenSpace(string input)
+{
+}
+*/
+
 void PrintMenu(string input)
 {
-    cout << "Yeah " << input << endl;
-}
+    char choice;
+    string searchString;
 
-void GetNumOfNonWSCharacters()
-{
-}
+    cout << "\nMENU" << endl;
+    cout << "c - Number of non-whitespace characters" << endl;
+    cout << "w - Number of words" << endl;
+    cout << "f - Find text" << endl;
+    cout << "r - Replace all !'s" << endl;
+    cout << "s - Shorten spaces" << endl;
+    cout << "q - Quit" << endl;
+    cout << endl;
+    cout << "Choose an option:" << endl;
 
-void GetNumOfWords()
-{
-}
+    cin >> choice;
 
-void FindText()
-{
-}
+    switch (choice)
+    {
+    case 'c':
+        cout << "Number of non-whitespace characters: " << GetNumOfNonWSCharacters(input) << endl;
+        PrintMenu(input);
+        break;
 
-void ReplaceExclamation()
-{
-}
+    case 'w':
+        cout << "Number of words: " << GetNumOfWords(input) << endl;
+        PrintMenu(input);
+        break;
 
-void ShortenSpace()
-{
+    case 'f':
+
+        cin.ignore();
+        cout << "Enter a word or phrase to be found:\n";
+        cin >> searchString;
+
+        cout << "\"" << searchString << "\""
+             << " instances: " << FindText(searchString, input) << endl;
+        PrintMenu(input);
+        break;
+        /*
+    case 'r':
+        ReplaceExclamation(input);
+        break;
+    case 's':
+        ShortenSpace(input);
+        break;
+        */
+    case 'q':
+        break;
+        
+    default:
+        break;
+    }
 }
 
 int main()
 {
     string input;
-    cin >> input;
+
+    cout << "Enter a sample text:\n"
+         << endl;
+
+    getline(cin, input);
+
+    cout << "You entered: " << input << endl;
+
     PrintMenu(input);
+    return 0;
 }
