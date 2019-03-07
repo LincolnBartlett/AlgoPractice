7679a32d7834065c39bb587e7bcb86c6c7b0bdaf
lincoln <lincolnbartlett@gmail.com>
2019-03-07 09:46:10 -0800

    in class

--- CurrentAssignment.cpp
+++ CurrentAssignment.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 struct movieData
 {
-    string title, director, runTime;
+    string title, director, year, runTime;
 
 }one,two;
 
 int main()
 {
-    string title, director, runTime;
-    getline(cin, title);
-    getline(cin, director);
-    getline(cin, runTime);
 
-    one.title == title;
-    one.director == director;
-    one.runTime == runTime;
 
-    getline(cin, title);
-    getline(cin, director);
-    getline(cin, runTime);
+    one.title = "The Funny Kids";
+    one.director = "Steven Shpeelberg";
+    one.year = "1990";
+    one.runTime = "200";
 
-    two.title == title;
-    two.director == director;
-    two.runTime == runTime;
+    two.title = "The Shining";
+    two.director = "Steven King";
+    two.year = "1980";
+    two.runTime = "190";
 
-    cout << one.title << "   " << two.title << endl;
+
+    cout << setw(20) << "Title" << setw(20) << "Director" << setw(20) << "Year Released" << setw(20)<< "Running Time" << endl;
+    cout << setw(20) << one.title << setw(20) << one.director << setw(20) << one.year << setw(20)<< one.runTime << endl;
+    cout << setw(20) << two.title << setw(20) << two.director << setw(20) << two.year << setw(20)<< two.runTime << endl;
+
+    return 0;
 }
